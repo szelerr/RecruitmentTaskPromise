e3ea6c0e04404bbb12c4cdfbd69c2b689cb327b4
szelerr <kuba.szelerski@outlook.com>
2024-12-11 20:03:54 +0100

    Added options, backdoor for wrong inputs

--- RecruitmentTaskPromise/Program.cs
+++ RecruitmentTaskPromise/Program.cs
@@ -4,29 +4,47 @@
     {
         static void Main(string[] args)
         {
+            string[] options = [
+                "1. Add product",
+                "2. Remove product",
+                "3. Display Basket",
+                "4. Exit"
+                ];
+
             bool isRunning = true;
+            int num;
 
             while (isRunning)
             {
                 Console.WriteLine("Welcome to our Tech Store! What do you want to do?");
 
-                int num;
+                foreach (string i in options)
+                {
+                    Console.WriteLine(i);
+                }
 
-                if (int.TryParse(Console.ReadLine(), out num))
+                bool isNum = int.TryParse(Console.ReadLine(), out num);
+
+                if (isNum && num > 0 && num <= options.Length)
                 {
                     switch (num)
                     {
                         case 1:
-                            return;
+                            break;
                         case 2:
-                            return;
+                            break;
                         case 3:
-                            return;
+                            break;
                         case 4:
                             Console.WriteLine("Shutting down...");
-                            isRunning = false;
                             return;
+                        default:
+                            break;
                     }
+                }
+                else
+                {
+                    Console.WriteLine("Unknown option. Try again");
                 }
             }
         }
