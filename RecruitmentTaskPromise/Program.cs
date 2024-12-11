794440be3ec078e688baac36b8dd2cb2952a2363
szelerr <kuba.szelerski@outlook.com>
2024-12-11 19:53:04 +0100

    Initial loop setup, exit condition

--- RecruitmentTaskPromise/Program.cs
+++ RecruitmentTaskPromise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Welcome to our Tech Store! What do you want to do?");
+
+                int num;
+
+                if (int.TryParse(Console.ReadLine(), out num))
+                {
+                    switch (num)
+                    {
+                        case 1:
+                            return;
+                        case 2:
+                            return;
+                        case 3:
+                            return;
+                        case 4:
+                            Console.WriteLine("Shutting down...");
+                            isRunning = false;
+                            return;
+                    }
+                }
+            }
         }
     }
 }
